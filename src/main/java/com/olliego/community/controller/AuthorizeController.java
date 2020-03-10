134058f099dc5bdd5dd6c1109fa6b09d0619ce5d
OllieGo <olliego@126.com>
2020-03-10 08:59:05 +0800

    'UserMapper'

--- src/main/java/com/olliego/community/controller/AuthorizeController.java
+++ src/main/java/com/olliego/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.olliego.community.controller;
 
 import com.olliego.community.dto.AccessTokenDTO;
 import com.olliego.community.dto.GithubUser;
+import com.olliego.community.mapper.UserMapper;
+import com.olliego.community.model.User;
 import com.olliego.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * @Classname AuthorizeController
@@ -33,6 +36,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+    
     @GetMapping("/callback")
     //HttpServletRequest request,spring自动把上下文中的request放入使用
     public String callback(@RequestParam(name = "code") String code,
@@ -45,10 +51,17 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if(user != null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登录成功，写cokkie和session
-            request.getSession().setAttribute("user", user);
+            request.getSession().setAttribute("user", githubUser);
             return "redirect:/";
 
         }else{
--- src/main/java/com/olliego/community/mapper/UserMapper.java
+++ src/main/java/com/olliego/community/mapper/UserMapper.java
@@ -1,10 +1,17 @@
 package com.olliego.community.mapper;
 
+import com.olliego.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * @Classname UserMapper
- * @Description TODO
+ * @Description 持久层
  * @Date 2020/3/9 15:11
  * @Created by OllieGo
  */
-public class UserMapper {
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
 }
