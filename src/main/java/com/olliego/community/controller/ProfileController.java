9da30074d77027f8e06ded05b7476c054b4d2638
OllieGo <olliego@126.com>
2020-03-14 16:53:29 +0800

    'profile'

--- src/main/java/com/olliego/community/controller/ProfileController.java
+++ src/main/java/com/olliego/community/controller/ProfileController.java
@@ -1,29 +1,27 @@
 package com.olliego.community.controller;
 
 import com.olliego.community.dto.PaginationDTO;
-import com.olliego.community.dto.QuestionDTO;
 import com.olliego.community.mapper.UserMapper;
-import com.olliego.community.model.Question;
 import com.olliego.community.model.User;
 import com.olliego.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
- * @Classname IndexController
- * @Description 主页
- * @Date 2020/3/7 11:26
+ * @Classname ProfileController
+ * @Description TODO
+ * @Date 2020/3/14 10:33
  * @Created by OllieGo
  */
 @Controller
-public class IndexController {
+public class ProfileController {
 
     @Autowired
     private UserMapper userMapper;
@@ -31,17 +29,19 @@ public class IndexController {
     @Autowired
     private QuestionService questionService;
 
-    @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
-                        @RequestParam(name = "page", defaultValue = "1") Integer page,
-                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
+    @GetMapping("/profile/{action}")
+    public String profile(HttpServletRequest request,
+                          @PathVariable(name = "action") String action,
+                          Model model,
+                          @RequestParam(name = "page", defaultValue = "1") Integer page,
+                          @RequestParam(name = "size", defaultValue = "5") Integer size){
+        User user = null;
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
+                    user = userMapper.findByToken(token);
                     if (user != null) {
                         request.getSession().setAttribute("user", user);
                     }
@@ -49,8 +49,20 @@ public class IndexController {
                 }
             }
         }
-        PaginationDTO pagination = questionService.list(page,size);
-        model.addAttribute("pagination", pagination);
-        return "index";
+
+        if(user == null){
+            return "redirect:/";
+        }
+
+        if("questions".equals(action)){
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "我的提问");
+        }else if("replies".equals(action)){
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
+        }
+        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+        model.addAttribute("pagination", paginationDTO);
+        return "profile";
     }
 }
