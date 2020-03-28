79bd48c2afc0a36dfa77b26d339f862f8c92d9cc
olliego <olliego@126.com>
2020-03-28 23:13:04 +0800

    related

--- src/main/java/com/olliego/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/olliego/community/mapper/QuestionExtMapper.java
@@ -8,8 +8,6 @@ import java.util.List;
 @Mapper
 public interface QuestionExtMapper {
     int incView(Question record);
-
     int incCommentCount(Question record);
-
     List<Question> selectRelated(Question question);
 }
